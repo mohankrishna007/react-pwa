bba4d0d358789411e969219b8dd442fe8a427747
Mohan Krishna <mohankrishna130101@gmail.com>
2022-11-03 20:27:55 +0530

    Logos

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,11 +2,10 @@ import * as React from "react";
 
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import '../styles/Header/Header.css'
 import { Link } from "react-router-dom";
+import LOGO from '../logo.svg';
+import { Image } from "react-bootstrap";
 
 export default function Header() {
 	const user = localStorage.getItem("token");
@@ -19,22 +18,12 @@ export default function Header() {
 return (
 	<AppBar position="sticky">
 		<Toolbar>
-		<IconButton
-			size="large"
-			edge="start"
-			color="inherit"
-			aria-label="menu"
-			sx={{ mr: 2 }}>
-			<MenuIcon />
-		</IconButton>
-		<Typography variant="h6"
-			component="div" sx={{ flexGrow: 1 }}>
-			Collegeportfolio
-		</Typography>
-			{
+		<Image src={LOGO} width={(window.innerWidth<768)?"50%": "20%"} height={"80%"}></Image>
+		<div style={{flexGrow:1}}></div>
+		{
 			user?<Link onClick={handleLogout}  className="btn btn-primary" style={{marginRight: '20px'}}>Logout</Link>:
 			<Link to="/register" className="btn btn-primary" style={{marginRight: '20px'}}>Register</Link>
-			}
+		}
 		</Toolbar>
 	</AppBar>
 );
