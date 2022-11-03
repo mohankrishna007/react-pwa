8fd558aad39b0bd9792fc2b743e4bb992d91e738
Mohan Krishna <mohankrishna130101@gmail.com>
2022-11-03 16:51:35 +0530

    Verification with mail

--- src/Authentication/EmailVerify.js
+++ src/Authentication/EmailVerify.js
@@ -1,28 +1,42 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import LinearProgress from '@mui/material/LinearProgress';
+import { Image } from "react-bootstrap";
+
 
 function EmailVerify() {
     const param = useParams();
 
-    const [data, setData] = useState("");
+    const [data, setData] = useState(null);
     
     useEffect(() => {
         const verifyEmailUrl = async () => {
 			try {
 				const url = `https://collegeportfoliobackendnode.azurewebsites.net/auth/users/${param.id}/verify/${param.token}`;
 				const resp =  await axios.get(url);
-                setData(JSON.stringify(resp.data));
+                setData(resp);
 			} catch (error) {
-				setData(JSON.stringify(error.response.data));
+				setData(error.response);
 			}
 		};
 		verifyEmailUrl();
-    }, [param])
+    }, [param.id, param.token])
 
     return(
         <div>
-            {data === ""?"Loading...": data}
+            {(data===null)?(
+                <div>
+                    <LinearProgress />
+                    <h1> Please Wait we are activating your Acount...</h1>
+                </div>
+            ):(
+                (data.status === 201)?(
+                    <Image style={{maxWidth: '90%', margin: '0 auto'}} src="https://cdn.dribbble.com/users/6659664/screenshots/15750262/group_13064_4x.png"></Image>
+                ):(
+                    <Image style={{maxWidth: '90%', margin: '0 auto'}} src="https://cdn.dribbble.com/users/1078347/screenshots/2799566/oops.png"></Image>
+                )
+            )}
         </div>
     )
 }
