b8b595f849a2b08f01fef0ca030f11c02612591f
Mohan Krishna <mohankrishna130101@gmail.com>
2022-11-08 23:26:21 +0530

    new COmmit

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -1,302 +1,122 @@
-import React, { useRef, useState } from "react";
-import { get, isEmpty, set } from "lodash-es";
-import { FormBuilder } from "@jeremyling/react-material-ui-form-builder";
-import {
-  Avatar,
-  Button,
-  IconButton,
-  InputAdornment,
-  Alert,
-  ThemeProvider,
-  createTheme,
-  TextField
-} from "@mui/material";
-import { LockOutlined, Visibility, VisibilityOff } from "@mui/icons-material";
-import { indigo, red } from "@mui/material/colors";
-import axios from "axios";
-import { useNavigate } from "react-router";
-import { MDBCard, MDBCardBody } from "mdb-react-ui-kit";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import RegisterTheme from "../Themes/RegisterTheme";
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import { MDBCard } from "mdbreact";
 
-async function validate(refs, form) {
-  for (const [attribute, ref] of Object.entries(refs.current)) {
-    var errors;
-    if (ref.validate) {
-      errors = await ref.validate(get(form, attribute));
-    }
-    if (!isEmpty(errors)) {
-      console.log(errors);
-      return false;
-    }
-  }
-  return true;
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
 export default function Login() {
-  const [message, setMessage] = React.useState("");
-  const [hideAlert, setHideAlert] = React.useState(true);
-  const [variant, setVariant] = React.useState("info");
-
-  const [userMail, setUserMail] = React.useState("");
-  const [sentMail, setSentMail] = useState(false);
-
-  const navigate = useNavigate();
-
-  const [form, setForm] = useState({});
-  const [showPassword, setShowPassword] = useState();
-  const [clickSubmit, setClickSubmit] = React.useState(false);
-  const [resetMessage, setResetMessage] = React.useState("");
-
-
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-    setSentMail(false);
-    setUserMail("");
-    setResetMessage("");
-  };
-
-  const refs = useRef({});
-
-  const updateForm = (updates) => {
-    const copy = { ...form };
-    for (const [key, value] of Object.entries(updates)) {
-      set(copy, key, value);
-    }
-    setForm(copy);
-  };
-
-  const handleResetMail = async () => {
-    if(sentMail){
-      handleClose();
-    }else{
-      try {
-        var data = {
-          email: userMail
-        }
-        var resp = await axios.post("https://collegeportfoliobackendnode.azurewebsites.net/auth/resetpassword", data);
-        setResetMessage("Password reset link sent successfully");
-        console.log(resp);
-      } catch (error) {
-        if(error.response.status === 500){
-          setResetMessage("Reset Link sent already")
-        }else{
-          setResetMessage("Password reset link sent successfully")
-        }
-        console.log(error.response);
-      }
-      setSentMail(true);
-    }
-  }
-
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const ok = await validate(refs, form);
-    if (!ok) {
-      return;
-    }
-
-    setClickSubmit(true);
-
-    try {
-      var resp = await axios.post(
-        "https://collegeportfoliobackendnode.azurewebsites.net/auth/login",
-        form
-      );
-      localStorage.setItem("token", JSON.stringify(resp.data));
-      localStorage.setItem("remember", JSON.stringify(form.remember))
-      console.log(JSON.stringify(resp.data));
-      navigate("/");
-      window.location.reload();
-    } catch (error) {
-      setMessage(error.response.data.message);
-      setVariant("info");
-      setHideAlert(false);
-      setClickSubmit(false);
-    }
-    console.log(form);
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
   };
-
-  const fields = [
-    {
-      component: "custom",
-      customComponent: () => (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Avatar style={{ backgroundColor: red[500], color: "white" }}>
-            <LockOutlined />
-          </Avatar>
-        </div>
-      ),
-    },
-    {
-      component: "display-text",
-      title: "Log In",
-      titleProps: {
-        style: {
-          fontSize: "20px",
-          fontWeight: "bold",
-        },
-      },
-      titleContainerProps: {
-        style: {
-          justifyContent: "center",
-        },
-      },
-    },
-    {
-      attribute: "email",
-      component: "text-field",
-      label: "Email",
-      props: {
-        required: true,
-      },
-      validations: {
-        required: true,
-      },
-    },
-    {
-      attribute: "password",
-      component: "text-field",
-      label: "Password",
-      props: {
-        type: showPassword ? "text" : "password",
-        InputProps: {
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <Visibility /> : <VisibilityOff />}
-              </IconButton>
-            </InputAdornment>
-          ),
-          style: {
-            paddingRight: 0,
-          },
-        },
-        required: true,
-      },
-      validations: {
-        required: true,
-        min: 8,
-        matches: ["/[a-z]/i", "At least 1 lowercase or uppercase letter"],
-        test: {
-          name: "specialChar",
-          test: (value) =>
-            /[0-9~!@#$%^&*()_+\-={}|[\]\\:";'<>?,./]/.test(value),
-          message: "At least 1 number or special character",
-        },
-      },
-    },
-    {
-      attribute: "remember",
-      component: "checkbox-group",
-      options: [
-        {
-          label: "Remember Me",
-          value: true,
-        },
-      ],
-      optionConfig: {
-        key: "label",
-        label: "label",
-        value: "value",
-      },
-    },
-  ];
 
   return (
     <ThemeProvider theme={createTheme(RegisterTheme)}>
-      <MDBCard style={{ width: "80%", margin: "0 auto" }}>
-        <MDBCardBody className="CardBody">
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <div style={{ width: "60%" }}>
-              <form onSubmit={handleSubmit}>
-                <FormBuilder
-                  fields={fields}
-                  form={form}
-                  updateForm={updateForm}
-                  refs={refs}
-                />
-                <Alert severity={variant} hidden={hideAlert}>
-                  {message}
-                </Alert>
-                <Button
+      <MDBCard>
+        <MDBCard>
+          <Container component="main" maxWidth="xs">
+            <Box
+              sx={{
+                marginTop: 8,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
                   fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
                   type="submit"
+                  fullWidth
                   variant="contained"
-                  color="primary"
-                  style={{ marginTop: "8px" }}
-                  disabled={clickSubmit}
+                  sx={{ mt: 3, mb: 2 }}
                 >
-                  Log In
+                  Sign In
                 </Button>
-              </form>
-              <div>
-                <Button
-                  onClick={() => handleClickOpen()}
-                  style={{
-                    textTransform: "initial",
-                    marginTop: "16px",
-                    color: indigo[500],
-                  }}
-                >
-                  Forgot Password?
-                </Button>
-              </div>
-              <div>
-                <Button
-                  onClick={() => navigate("/register")}
-                  style={{
-                    textTransform: "initial",
-                    color: indigo[500],
-                  }}
-                >
-                  Don't have an account?
-                </Button>
-              </div>
-            </div>
-          </div>
-        </MDBCardBody>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/register" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+          </Container>
+        </MDBCard>
       </MDBCard>
-
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle><b>Please Enter Your Email</b></DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-              Password reset link will be sent to your email address.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            value={userMail}
-            onChange={(e) => setUserMail(e.target.value)}
-            variant="standard"
-          />
-          <br/><br/>
-          <Alert severity="info" hidden={!sentMail}>{resetMessage}</Alert>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleResetMail}>{(!sentMail)?"Send email": "OK"}</Button>
-        </DialogActions>
-      </Dialog>
-
     </ThemeProvider>
   );
 }
--- src/Authentication/Register.js
+++ src/Authentication/Register.js
@@ -1,202 +1,121 @@
-import React, { useRef, useState } from "react";
-import { get, isEmpty, set } from "lodash-es";
-import { FormBuilder } from "@jeremyling/react-material-ui-form-builder";
-import {
-  Avatar,
-  Button,
-  IconButton,
-  InputAdornment,
-  Alert,
-  ThemeProvider,
-  createTheme,
-} from "@mui/material";
-import { LockOutlined, Visibility, VisibilityOff } from "@mui/icons-material";
-import { indigo, red } from "@mui/material/colors";
-import axios from "axios";
-import { useNavigate } from "react-router";
-import { MDBCard, MDBCardBody } from "mdb-react-ui-kit";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import RegisterTheme from "../Themes/RegisterTheme";
+import { MDBCard } from "mdbreact";
 
-async function validate(refs, form) {
-  for (const [attribute, ref] of Object.entries(refs.current)) {
-    var errors;
-    if (ref.validate) {
-      errors = await ref.validate(get(form, attribute));
-    }
-    if (!isEmpty(errors)) {
-      console.log(errors);
-      return false;
-    }
-  }
-  return true;
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
 export default function Register() {
-  const [message, setMessage] = React.useState("");
-  const [hideAlert, setHideAlert] = React.useState(true);
-  const [variant, setVariant] = React.useState("info");
-  const [clickSubmit, setClickSubmit] = React.useState(false);
-
-  const navigate = useNavigate();
-
-  const [form, setForm] = useState({});
-  const [showPassword, setShowPassword] = useState();
-
-  const refs = useRef({});
-
-  const updateForm = (updates) => {
-    const copy = { ...form };
-    for (const [key, value] of Object.entries(updates)) {
-      set(copy, key, value);
-    }
-    setForm(copy);
-  };
-
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const ok = await validate(refs, form);
-    if (!ok) {
-      return;
-    }
-
-    setClickSubmit(true);
-    try {
-      var resp = await axios.post(
-        "https://collegeportfoliobackendnode.azurewebsites.net/auth/register",
-        form
-      );
-      console.log(JSON.stringify(resp.data));
-      setMessage(resp.data.message);
-      setVariant("success");
-      setHideAlert(false);
-    } catch (error) {
-      if (error.response) {
-        setMessage(error.response.data.message);
-        setVariant("error");
-        setHideAlert(false);
-        console.log(JSON.stringify(error.response.data));
-        setClickSubmit(false);
-      }
-    }
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
   };
-
-  const fields = [
-    {
-      component: "custom",
-      customComponent: () => (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Avatar style={{ backgroundColor: red[500], color: "white" }}>
-            <LockOutlined />
-          </Avatar>
-        </div>
-      ),
-    },
-    {
-      component: "display-text",
-      title: "Sign up",
-      titleProps: {
-        style: {
-          fontSize: "20px",
-          fontWeight: "bold",
-        },
-      },
-      titleContainerProps: {
-        style: {
-          justifyContent: "center",
-        },
-      },
-    },
-    {
-      attribute: "email",
-      component: "text-field",
-      label: "Email",
-      props: {
-        required: true,
-      },
-      validations: {
-        required: true,
-      },
-    },
-    {
-      attribute: "password",
-      component: "text-field",
-      label: "Password",
-      props: {
-        type: showPassword ? "text" : "password",
-        InputProps: {
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <Visibility /> : <VisibilityOff />}
-              </IconButton>
-            </InputAdornment>
-          ),
-          style: {
-            paddingRight: 0,
-          },
-        },
-        required: true,
-      },
-      validations: {
-        required: true,
-        min: 8,
-        matches: ["/[a-z]/i", "At least 1 lowercase or uppercase letter"],
-        test: {
-          name: "specialChar",
-          test: (value) =>
-            /[0-9~!@#$%^&*()_+\-={}|[\]\\:";'<>?,./]/.test(value),
-          message: "At least 1 number or special character",
-        },
-      },
-    },
-  ];
 
   return (
     <ThemeProvider theme={createTheme(RegisterTheme)}>
-      <MDBCard style={{ maxWidth: "80%", margin: "0 auto" }}>
-        <MDBCardBody className="CardBody">
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <div style={{ width: "60%" }}>
-              <form onSubmit={handleSubmit}>
-                <FormBuilder
-                  fields={fields}
-                  form={form}
-                  updateForm={updateForm}
-                  refs={refs}
-                />
-                <Alert severity={variant} hidden={hideAlert}>
-                  {message}
-                </Alert>
-                <br />
-                <Button
+      <MDBCard>
+        <MDBCard>
+          <Container component="main" maxWidth="xs">
+            <Box
+              sx={{
+                marginTop: 8,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign Up
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
                   fullWidth
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  style={{ marginTop: "8px" }}
-                  disabled={clickSubmit}
-                >
-                  Sign Up
-                </Button>
-              </form>
-              <div>
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
                 <Button
-                  onClick={() => navigate("/login")}
-                  style={{
-                    textTransform: "initial",
-                    marginTop: "16px",
-                    color: indigo[500],
-                  }}
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
                 >
-                  Already have an account?
+                  Sign In
                 </Button>
-              </div>
-            </div>
-          </div>
-        </MDBCardBody>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="login" variant="body2">
+                      {"Already have an account? Login In"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+          </Container>
+        </MDBCard>
       </MDBCard>
     </ThemeProvider>
   );
